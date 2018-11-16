6d783b1928819ec5d553a56da7864e602aab87f2
Lupo <fasmusic@gmail.com>
2018-11-15 19:02:13 -0500

    Working Background

---
+++ src/Components/BackgroundImage/BackgroundImage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import background from '../../IMG/InReviewBACKGROUNDlow.jpg';
+//import style from 'BackgroundImage.module.css';
+
+
+
+const backgroundImage = () => (
+    <div >
+
+
+        <p>blah</p>
+    </div>
+);
+
+export default backgroundImage;
---
+++ src/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Layout.module.css';
+
+const layout = () => (
+    <div className={style.background}>
+
+
+
+    </div>
+);
+
+export default layout;
